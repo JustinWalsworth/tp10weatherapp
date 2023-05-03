8f1504c353313d6d7dda606431649c4e3df733e1
JustinWalsworth <justin.walsworth@wsu.edu>
2023-05-03 21:08:40 +0000

    transfer of more files

--- scripts.js
+++ scripts.js
@@ -17,6 +17,11 @@ document.addEventListener("DOMContentLoaded", function() {
         .then(response => response.json())
         .then(function(response){
             document.querySelector("#location span").innerHTML = response.location.name;
+            document.querySelector("#tempf span").innerHTML = response.current.temp_f;
+            document.querySelector("#winddir span").innerHTML = response.current.wind_dir;
+            document.querySelector("#windspeed span").innerHTML = response.current.wind_mph;
+            document.querySelector("#cloudcover span").innerHTML = response.current.vis_miles;
+            document.querySelector("#humidity span").innerHTML = response.current.humidity;
             
             return response;
         })
