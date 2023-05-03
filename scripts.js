5b32a812ac423d705b9ed00a349a88b16402106d
JustinWalsworth <justin.walsworth@wsu.edu>
2023-05-03 21:32:56 +0000

    transfering displayed api

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@
 // adds the code to wait for the DOM
 document.addEventListener("DOMContentLoaded", function() {
     
-    // code to load API
+    // code to load the weather API
     const options = {
 	    method: 'GET',
 	    headers: {
@@ -12,16 +12,16 @@ document.addEventListener("DOMContentLoaded", function() {
 	    }
     };
     
-    // fetches data for zipcode 99163
+    // fetches data for zipcode 99163 which is Pullman, WA
     fetch('https://weatherapi-com.p.rapidapi.com/current.json?q=99163', options)
         .then(response => response.json())
         .then(function(response){
-            document.querySelector("#location span").innerHTML = response.location.name;
-            document.querySelector("#tempf span").innerHTML = response.current.temp_f;
-            document.querySelector("#winddir span").innerHTML = response.current.wind_dir;
-            document.querySelector("#windspeed span").innerHTML = response.current.wind_mph;
-            document.querySelector("#cloudcover span").innerHTML = response.current.vis_miles;
-            document.querySelector("#humidity span").innerHTML = response.current.humidity;
+            document.querySelector("#location span").innerHTML = response.location.name; // selects the location
+            document.querySelector("#tempf span").innerHTML = response.current.temp_f; // selects the temerature in f
+            document.querySelector("#winddir span").innerHTML = response.current.wind_dir; // selects the wind direction
+            document.querySelector("#windspeed span").innerHTML = response.current.wind_mph; // selects the wind speed
+            document.querySelector("#cloudcover span").innerHTML = response.current.vis_miles; // selects the cloud cover
+            document.querySelector("#humidity span").innerHTML = response.current.humidity; // selects the humidity
             
             return response;
         })
